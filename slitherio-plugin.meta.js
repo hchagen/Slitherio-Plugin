49c57f1794d2eb4c213f394ee21d46822399c749
Montaron <hchagen@live.no>
2016-04-07 22:48:57 +0200

    Added autoupdate

--- slitherio-plugin.meta.js
+++ slitherio-plugin.meta.js
@@ -4,6 +4,8 @@
 // @description Plugin for slither.io
 // @include     http://slither.io/*
 // @include     http://slither.io/*
+// @updateURL   https://github.com/Montaron/Slitherio-Plugin/raw/master/slitherio-plugin.meta.js
+// @downloadURL https://github.com/Montaron/Slitherio-Plugin/raw/master/slitherio-plugin.user.js
 // @version     1.11
 // @grant       none
 // ==/UserScript==

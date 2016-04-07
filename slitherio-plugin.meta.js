b6c4b562b9206c4e450da067baa4d0dc6cf3b9d8
Montaron <hchagen@live.no>
2016-04-07 11:26:57 +0200

    The meta-file used by ***monkey for automatic updates etc.

---
+++ slitherio-plugin.meta.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        slitherio plugin
+// @namespace   hc.priv.no
+// @description Plugin for slither.io
+// @include     http://slither.io/*
+// @version     1.1
+// @grant       none
+// ==/UserScript==

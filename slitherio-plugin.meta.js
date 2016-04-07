000a423289a72aeebf072bdf4bce6acf1d5e3052
Montaron <hchagen@live.no>
2016-04-07 12:15:49 +0200

    Added https
    // @include     http://slither.io/*

--- slitherio-plugin.meta.js
+++ slitherio-plugin.meta.js
@@ -3,6 +3,7 @@
 // @namespace   hc.priv.no
 // @description Plugin for slither.io
 // @include     http://slither.io/*
+// @include     http://slither.io/*
 // @version     1.1
 // @grant       none
 // ==/UserScript==
